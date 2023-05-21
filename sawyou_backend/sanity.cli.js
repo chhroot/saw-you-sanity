3a99f23a02b6a9d2532c5cddb01bb839afd0bbd5
Amruth Kiran <amruthkiran47@gmail.com>
2023-05-21 19:32:06 +0530

    added google oauth

---
+++ sawyou_backend/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'hwnz5ft7',
+    dataset: 'production'
+  }
+})
---
+++ sawyou_backend/sanity.config.js
@@ -0,0 +1,23 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+//import {googleMapsInput} from '@sanity/google-maps-input'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'sawyou_backend',
+
+  projectId: 'hwnz5ft7',
+  dataset: 'production',
+
+  plugins: [
+    deskTool(),
+    visionTool(),
+    //googleMapsInput(),
+  ],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
