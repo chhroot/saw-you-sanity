cebd2997000735f4e076b3c13bd6cdc6fa0fec37
Amruth Kiran <amruthkiran47@gmail.com>
2023-05-26 00:15:38 +0530

    save functionality

--- sawyou_frontend/src/components/Pin.jsx
+++ sawyou_frontend/src/components/Pin.jsx
@@ -1,12 +1,96 @@
-import React from 'react'
-import { urlFor } from '../client'
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
+import { FaDownload } from 'react-icons/fa';
+import { AiTwotoneDelete } from 'react-icons/ai';
+import { BsFillArrowUpRightCircleFill } from 'react-icons/bs';
 
-const Pin = ({ pin: { postedBy, image, _id, destination }}) => {
+import { client, urlFor } from '../client';
+import { fetchUser } from '../utils/fetchUser';
+
+const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
+  const [postHovered, setPostHovered] = useState(false);
+  const [savingPost, setSavingPost] = useState(false);
+  const navigate = useNavigate();
+  const user = fetchUser();
+
+  const alreadySaved = !!save?.filter((item) => item.postedBy._id === user.sub)
+    ?.length;
+
+  const savePin = (id) => {
+    if (!alreadySaved) {
+      setSavingPost(true);
+
+      client
+        .patch(id)
+        .setIfMissing({ save: [] })
+        .insert('after', 'save[-1]', [
+          {
+            _key: uuidv4(),
+            userId: user.sub,
+            postedBy: { _type: 'postedBy', _ref: user.sub },
+          },
+        ])
+        .commit();
+    }
+  };
+
   return (
-    <div>
-        <img className="rounded-lg w-full" src={urlFor(image).width(250).url()} alt="user-post" />
+    <div className="m-2">
+      <div
+        onMouseEnter={() => setPostHovered(true)}
+        onMouseLeave={() => setPostHovered(false)}
+        onClick={() => navigate(`/pin-detail/${_id}`)}
+        className="relative cursor-arrow w-auto hover:shadow-lg rounded-lg overflow-hidden transition-all duration-500 ease-in-out"
+      >
+        <img
+          className="rounded-lg w-full"
+          src={urlFor(image).width(250).url()}
+          alt="user-post"
+        />
+
+        {postHovered && (
+          <div
+            className="absolute top-0 w-full h-full flex flex-col justify-between p-1 pr-2 pt-2 pb-2 pb-2 w-full h-full"
+            style={{ height: '100%' }}
+          >
+            <div className="flex items-center justify-between">
+              <div className="flex gap-2">
+                <a
+                  href={`${image?.asset?.url}?dl=`}
+                  download
+                  onClick={(e) => e.stopPropagation()}
+                  className="bg-white h-9 w-9 rounded-full items-center flex justify-center text-xl opacity-75 hover:opacity-100 hover:shadow-md"
+                >
+                  <FaDownload />
+                </a>
+              </div>
+
+              {alreadySaved ? (
+                <button
+                  type="button"
+                  className="bg-red-500 opacity-70 hover:opacity-100 text-white px-5 py-1 rounded-3xl outline-none txt-base font-bold hover:shadow-md"
+                >
+                  {save?.length} Saved
+                </button>
+              ) : (
+                <button
+                  onClick={(e) => {
+                    stopPropagation();
+                    savePin(_id);
+                  }}
+                  type="button"
+                  className="bg-red-500 opacity-70 hover:opacity-100 text-white px-5 py-1 rounded-3xl outline-none txt-base font-bold hover:shadow-md"
+                >
+                  Save
+                </button>
+              )}
+            </div>
+          </div>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Pin;
